37df2a923f1c7bc3218b0f8754d7a205b3a05bf6
Y3rf <90943494+Y3rf@users.noreply.github.com>
2021-09-23 19:15:36 +0530

    LS

---
+++ ListSlices/LS.py
@@ -0,0 +1,27 @@
+#############################################
+yerf_nums = [0,1,4,9,19,34,12,54,77,7]
+print(yerf_nums[2:6])
+print(yerf_nums[6:7])
+print(yerf_nums)
+#############################################
+
+print(yerf_nums[:7])
+print(yerf_nums[7:])
+
+#############################################
+
+print(yerf_nums[::2])
+print(yerf_nums[2:8:3])
+
+#############################################
+
+print(yerf_nums[::-1])
+print(yerf_nums[-1])
+print(yerf_nums[-4])
+print(yerf_nums[:])
+print(yerf_nums[:-3])
+print(yerf_nums[6:4:-1])
+
+######################END####################
+
+
